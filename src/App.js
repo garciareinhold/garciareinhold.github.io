61583e50e2b8b9e294db91164bc050581da7e396
garciareinhold <arturogreinhold@gmail.com>
2022-05-30 12:22:07 -0300

    deploy

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [pokemon, setPokemon] = useState('');
+
+  const pokeApiUrl = ' https://pokeapi.co/api/v2/pokemon/';
+
+  const getRemainingDays = () => {
+    const date1 = new Date();
+    const date2 = new Date('11/04/2022');
+    const diffTime = Math.abs(date2 - date1);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    return diffDays;
+  }
+
+  const getPokemonImg = () => {
+    return pokemon?.sprites?.front_default;
+  }
+
+  useEffect(() => {
+    const days = getRemainingDays()
+    fetch(`${ pokeApiUrl }${ days }`)
+      .then((response) => response.json())
+      .then((poke) => {
+        setPokemon(poke)
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Faltan { getRemainingDays() } dias
+        <img src={ getPokemonImg() } className="App-logo" alt="logo" />
+        <p>{ pokemon?.name?.toUpperCase() }</p>
       </header>
     </div>
   );
